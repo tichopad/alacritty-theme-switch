6b63dd1902f0968f26a26737a9de8cbfe859d5d2
Michael Tichopad <michael@tichopad.dev>
2023-05-27 01:10:07 +0200

    Tests WIP

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,20 +1,122 @@
-import { expect, it } from "vitest";
-import { parseYaml } from "./main.js";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import fs from "fs/promises";
+import yaml, { YAMLSeq, parseDocument } from "yaml";
+import {
+  modifyConfig,
+  listYamlFiles,
+  modifyImportList,
+  writeConfig,
+} from "./main.js";
 
-const EXAMPLE_YAML = `
-#This is an example YAML file
-name: John
-age: 30
-cars:
-  - Ford #Commented
-  - BMW
-  - Fiat
-`;
+function dd(strings: TemplateStringsArray, ...values: unknown[]): string {
+  let result = "";
+  for (let i = 0; i < strings.length; i++) {
+    result += strings[i] + (values[i] || "");
+  }
+  const lines = result.split("\n");
+  const minIndent = Math.min(
+    ...lines
+      .filter((line) => line.trim() !== "")
+      .map((line) => line.search(/\S/))
+  );
+  return lines
+    .map((line) => line.slice(minIndent))
+    .join("\n")
+    .trim()
+    .concat("\n");
+}
 
-it("should parse YAML", () => {
-  expect(parseYaml(EXAMPLE_YAML)).toEqual({
-    name: "John",
-    age: 30,
-    cars: ["Ford", "BMW", "Fiat"],
+describe("listYamlFiles", () => {
+  it("lists theme files", async () => {
+    const files = await listYamlFiles(`${__dirname}/themes`);
+    expect(files).toEqual([
+      "/home/michael/Projekty/alacritty-theme-switch/src/themes/cobalt2.yml",
+      "/home/michael/Projekty/alacritty-theme-switch/src/themes/mariana.yml",
+    ]);
+  });
+});
+
+describe("modifyImportList", () => {
+  it("should add themeFilePath to import list", () => {
+    const inputYaml = dd`
+      import:
+        - file1
+        - file2
+    `;
+    const themeFilePath = "themeFile";
+    const expectedYaml = dd`
+      import:
+        - file1
+        - file2
+        - themeFile
+    `;
+    const doc = parseDocument(inputYaml);
+    const modifiedDoc = modifyImportList(themeFilePath)(doc);
+    expect(modifiedDoc.toString()).toEqual(expectedYaml);
+  });
+
+  it("should create import list if it does not exist", () => {
+    const inputYaml = dd`
+      key: value
+    `;
+    const themeFilePath = "themeFile";
+    const expectedYaml = dd`
+      key: value
+      import:
+        - themeFile
+    `;
+    const doc = parseDocument(inputYaml);
+    const modifiedDoc = modifyImportList(themeFilePath)(doc);
+    expect(modifiedDoc.toString()).toEqual(expectedYaml);
+  });
+});
+
+describe("modifyConfig", () => {
+  it("should return a modified yaml string", () => {
+    const alacrittyConfig = dd`
+      window:
+        opacity: 1.0
+      import:
+        - file1
+    `;
+    const themeFilePath = "themeFile";
+    const expectedYaml = dd`
+      window:
+        opacity: 1.0
+      import:
+        - file1
+        - themeFile
+    `;
+    expect(modifyConfig(alacrittyConfig, themeFilePath)).toEqual(expectedYaml);
+  });
+});
+
+describe("writeConfig", () => {
+  const CONFIG_PATH = `${__dirname}/_tmp_test_config.yml`;
+  beforeEach(async () => {
+    const configYaml = dd`
+      window:
+        opacity: 1.0
+      import:
+        - file1
+    `;
+    await fs.writeFile(CONFIG_PATH, configYaml);
+  });
+  afterEach(async () => {
+    await fs.unlink(CONFIG_PATH);
+  });
+  it("should modify and write a config file", async () => {
+    const themeFilePath = `${__dirname}/themes/cobalt2.yml`;
+    const configPath = CONFIG_PATH;
+    const expectedYaml = dd`
+      window:
+        opacity: 1.0
+      import:
+        - file1
+        - ${themeFilePath}
+    `;
+    await writeConfig(configPath, themeFilePath);
+    const outputYaml = await fs.readFile(configPath, "utf8");
+    expect(outputYaml).toEqual(expectedYaml);
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,70 @@
-import { parse } from "yaml";
+import fs, { readFile } from "fs/promises";
+import path from "path";
+import { pipe } from "fp-ts/function";
+import { stringify, type Document, parseDocument, isSeq } from "yaml";
 
-export function parseYaml(yamlString: string) {
-  return parse(yamlString);
+type Brand<K, T> = K & { __brand: T };
+
+export type AbsolutePath = Brand<string, "AbsolutePath">;
+
+function isAbsolutePath(filePath: string): filePath is AbsolutePath {
+  return path.isAbsolute(filePath);
 }
+
+function getAbsolutePath(...pathSegments: string[]): AbsolutePath {
+  return path.resolve(...pathSegments) as AbsolutePath;
+}
+
+export async function listYamlFiles(
+  directory: string
+): Promise<AbsolutePath[]> {
+  const files = await fs.readdir(directory);
+
+  return files
+    .filter((file) => /\.ya?ml/i.test(path.extname(file)))
+    .map((file) => getAbsolutePath(directory, file));
+}
+
+const loadYamlFile = (filePath: string): Promise<string> =>
+  readFile(filePath, "utf8");
+
+const parseYaml = (yamlString: string): Document.Parsed =>
+  parseDocument(yamlString);
+
+const serializeYaml = (doc: Document): string => stringify(doc);
+
+export const modifyImportList =
+  (themeFilePath: string) =>
+  (doc: Document.Parsed): Document => {
+    const clonedDoc = doc.clone();
+    let importList = clonedDoc.get("import");
+    if (!importList) {
+      importList = clonedDoc.createNode([]);
+    }
+    if (!isSeq(importList)) {
+      throw new Error("Import list is not a list");
+    }
+    importList.add(themeFilePath);
+    clonedDoc.set("import", importList);
+    return clonedDoc;
+  };
+
+export const modifyConfig = (
+  alacrittyConfig: string,
+  themeFilePath: string
+): string =>
+  pipe(
+    alacrittyConfig,
+    parseYaml,
+    modifyImportList(themeFilePath),
+    serializeYaml
+  );
+
+export const writeConfig = async (
+  configPath: string,
+  themeFilePath: string
+) => {
+  const alacrittyConfig = await loadYamlFile(configPath);
+  const modifiedConfig = modifyConfig(alacrittyConfig, themeFilePath);
+  await fs.writeFile(configPath, modifiedConfig);
+};
