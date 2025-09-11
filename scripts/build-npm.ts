77171111ae71cc49588635f41cedfdbccdca8bf4
Michael Tichopad <michael@tichopad.dev>
2025-09-11 22:47:04 +0200

    Add build script for NPM/Node compatible output

---
+++ scripts/build-npm.ts
@@ -0,0 +1,35 @@
+import { build, emptyDir } from "@deno/dnt";
+import denoJson from "../deno.json" with { type: "json" };
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./src/main.ts"],
+  outDir: "./npm",
+  shims: {
+    deno: true,
+  },
+  scriptModule: false,
+  package: {
+    // package.json properties
+    name: "alacritty-theme-switch",
+    version: denoJson.version,
+    description: "CLI utility for switching Alacritty color themes",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/tichopad/alacritty-theme-switch.git",
+    },
+    bugs: {
+      url: "https://github.com/tichopad/alacritty-theme-switch/issues",
+    },
+  },
+  compilerOptions: {
+    lib: ["DOM", "ESNext"],
+  },
+  postBuild() {
+    // steps to run after building and before running the tests
+    Deno.copyFileSync("LICENSE.md", "npm/LICENSE.md");
+    Deno.copyFileSync("README.md", "npm/README.md");
+  },
+});
